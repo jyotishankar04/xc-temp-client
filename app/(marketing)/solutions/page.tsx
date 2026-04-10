2961c9279a517ca2cb4c9b4dea57daa20230e5d6
Sravesh <sraveshnandan@gmail.com>
2026-04-11 02:33:27 +0530

    fixed prod err

---
+++ app/(marketing)/solutions/page.tsx
@@ -0,0 +1,11 @@
+import { ComingSoon } from "@/components/marketing/coming-soon";
+
+const SolutionsPage = () => {
+  return (
+    <div>
+      <ComingSoon page="solutions" />
+    </div>
+  );
+};
+
+export default SolutionsPage;
